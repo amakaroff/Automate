5bf07220aae6793a012f8222d7dab96bf6ac9a2a
amakaroff <alekseymakarov1996@gmail.com>
2017-10-24 18:38:24 +0400

    Little fixes

--- src/main/java/org/makarov/task/TaskTree.java
+++ src/main/java/org/makarov/task/TaskTree.java
@@ -1,5 +1,18 @@
 package org.makarov.task;
 
 
+import org.makarov.automate.Automate;
+import org.makarov.util.Functions;
+import org.makarov.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class TaskTree {
+
+    public static Collection<Pair<String, String>> getLexems(String line) {
+        Collection<Automate<String>> automates = new ArrayList<>();
+
+        return Functions.getLexems(automates, line);
+    }
 }
