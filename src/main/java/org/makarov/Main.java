871f48dd0e67cb90310c0bab2a2aaecbf1af1e62
amakaroff <alekseymakarov1996@gmail.com>
2017-11-14 12:42:37 +0300

    Fixes

--- src/main/java/org/makarov/Main.java
+++ src/main/java/org/makarov/Main.java
@@ -3,6 +3,9 @@ package org.makarov;
 import org.makarov.automate.Automate;
 import org.makarov.automate.serialize.AutomateSerializer;
 import org.makarov.automate.serialize.AutomateToJSONSerializer;
+import org.makarov.task.TaskTree;
+import org.makarov.util.FileReader;
+import org.makarov.util.Functions;
 import org.makarov.util.optimization.AutomateOptimizationUtils;
 import org.makarov.util.parser.LexerParser;
 import org.makarov.util.transformer.AutomateTransformer;
@@ -16,13 +19,22 @@ public class Main {
         AutomateSerializer serializer = new AutomateToJSONSerializer();
         Collection<Automate> automates = new ArrayList<>();
 
+        Automate automate1 = null;
         for (Automate automate : LexerParser.getAutomates("lexic/lexer.lex")) {
+            automate1 = automate;
             automate = AutomateTransformer.toDeterministicAutomateTransform(automate);
             AutomateOptimizationUtils.optimization(automate);
             automates.add(automate);
         }
 
+        //01011010001010
+        //01011111000101
         System.out.println(automates);
+
+        System.out.println(Functions.function(automate1, "01011111000101", 0));
+
+        //System.out.println(TaskTree.getLexemes(FileReader.readFile("code.txt")));
+        //System.out.println(Functions.getLexemes(automates, FileReader.readFile("code.txt")));
     }
 
     //Regular helpers
--- src/main/java/org/makarov/util/optimization/AutomateOptimizationUtils.java
+++ src/main/java/org/makarov/util/optimization/AutomateOptimizationUtils.java
@@ -1,17 +1,18 @@
 package org.makarov.util.optimization;
 
 import org.makarov.automate.Automate;
+import org.makarov.automate.DeterministicAutomate;
+import org.makarov.automate.serialize.AutomateToStringSerializer;
 import org.makarov.util.AutomateReflection;
 import org.makarov.util.operations.AutomateRenamer;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class AutomateOptimizationUtils {
+
+    private static final String NON_EQUALS_SYMBOL = "X";
+
+    private static final String EQUALS_SYMBOL = "O";
 
     @SuppressWarnings("unchecked")
     public static void optimization(Automate automate) {
@@ -88,7 +89,96 @@ public class AutomateOptimizationUtils {
             }
         }
 
+        System.out.println(automate.toString());
+
+        if (automate instanceof DeterministicAutomate) {
+            deleteEqualsState(reflection);
+        }
+
         AutomateRenamer.renameAutomate(automate);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static DeterministicAutomate deleteEqualsState(AutomateReflection automate) {
+        Map<String, Map<String, String>> checkMap = new HashMap<>();
+        Set<String> endStates = automate.getEndStates();
+        Set<String> states = new HashSet<>(automate.getTransitions().keySet());
+        Map<String, Map<String, Object>> transitions = automate.getTransitions();
+        List<String> alphabet = automate.getAlphabet();
+
+        for (String state : states) {
+            Map<String, String> innerMap = new HashMap<>();
+            for (String innerState : states) {
+                if (state.equals(innerState)) {
+                    innerMap.put(innerState, EQUALS_SYMBOL);
+                } else if (endStates.contains(state) && !endStates.contains(innerState)
+                        || !endStates.contains(state) && endStates.contains(innerState)) {
+                    innerMap.put(innerState, NON_EQUALS_SYMBOL);
+                } else {
+                    innerMap.put(innerState, "");
+                }
+            }
+
+            checkMap.put(state, innerMap);
+        }
+
+        for (Map<String, String> stringStringMap : checkMap.values()) {
+            System.out.println(stringStringMap);
+        }
+
+        boolean isEndChecking = false;
+
+        while (!isEndChecking) {
+            isEndChecking = true;
+            for (String state : states) {
+                for (String innerState : states) {
+                    for (String letter : alphabet) {
+                        if (!checkMap.get(state).get(innerState).equals(NON_EQUALS_SYMBOL)) {
+                            String firstState = String.valueOf(transitions.get(state).get(letter));
+                            String secondState = String.valueOf(transitions.get(innerState).get(letter));
+
+                            if (!firstState.equals("null") && !secondState.equals("null") &&
+                                    NON_EQUALS_SYMBOL.equals(checkMap.get(firstState).get(secondState))) {
+                                isEndChecking = false;
+                                checkMap.get(state).put(innerState, NON_EQUALS_SYMBOL);
+                                checkMap.get(innerState).put(state, NON_EQUALS_SYMBOL);
+                                break;
+                            }
+                        }
+                    }
+
+                    if (!checkMap.get(innerState).get(state).equals(NON_EQUALS_SYMBOL)) {
+                        checkMap.get(innerState).put(state, EQUALS_SYMBOL);
+                        checkMap.get(state).put(innerState, EQUALS_SYMBOL);
+                    }
+                }
+            }
+        }
+
+        for (String state : states) {
+            for (String innerState : states) {
+                if (checkMap.get(state).get(innerState).equals(EQUALS_SYMBOL)) {
+                    checkMap.get(innerState).put(state, NON_EQUALS_SYMBOL);
+                }
+            }
+        }
+
+        for (String state : states) {
+            for (String innerState : states) {
+                if (checkMap.get(state).get(innerState).equals(EQUALS_SYMBOL)) {
+                    if (automate.getBeginState().equals(state)) {
+                        automate.setBeginState(innerState);
+                    }
+
+                    if (endStates.contains(state) && endStates.contains(innerState)
+                            || !endStates.contains(state) && !endStates.contains(innerState)) {
+                        removeState(state, innerState, transitions);
+                    }
+                }
+            }
+        }
+
+        return (DeterministicAutomate) automate.getAutomate();
     }
 
 
